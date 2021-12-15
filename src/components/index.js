25492eb0ccb3bc4ada8ae5da8d8d86c34a8c546e
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-12-15 23:38:32 +0600

    added some css

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export {default as article} from './article/article.jsx';
 export {default as brand} from './brand/brand.jsx';
 export {default as cta} from './cta/cta.jsx';
 export {default as feature} from './feature/feature.jsx';
-export {default as Navbar} from './navbar/navbar.jsx';
+export {default as Navbar} from './navbar/Navbar.jsx';
